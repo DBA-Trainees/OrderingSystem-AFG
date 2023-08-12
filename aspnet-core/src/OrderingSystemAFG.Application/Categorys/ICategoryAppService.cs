68d4e18f968578139650ae769541d289609d98da
Adrienne Garimbao <Adriennegarimbao@hotmail.com>
2023-08-12 20:28:15 +0800

    Added IAppService for Category

---
+++ aspnet-core/src/OrderingSystemAFG.Application/Categorys/ICategoryAppService.cs
@@ -0,0 +1,20 @@
+﻿using Abp.Application.Services;
+using Abp.Application.Services.Dto;
+using OrderingSystemAFG.Categorys.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderingSystemAFG.Categorys
+{
+
+    public interface ICategoryAppService : IAsyncCrudAppService<CategoryDto, int, PagedCategoryResultRequestDto, CreateCategoryDto, CategoryDto>
+    {
+        //Task<PagedResultDto<CategoryDto>> GetAllAsync(PagedCategoryResultRequestDto input);
+
+        //Task<List<CategoryDto>> GetAllTheListOfCategoryFromDTO();
+
+    }
+}
