c57a14ca17c34147fd93bcf5746d6fe7c8e5aa21
Adrienne Garimbao <Adriennegarimbao@hotmail.com>
2023-08-14 19:38:49 +0800

    Added Entity Class for Food Type

---
+++ aspnet-core/src/OrderingSystemAFG.Core/Entities/Type.cs
@@ -0,0 +1,17 @@
+﻿using Abp.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderingSystemAFG.Entities
+{
+    public class Type : FullAuditedEntity<int>
+    {
+        /* List Columns */
+
+        public int FoodTypeName { get; set; }
+
+    }
+}
