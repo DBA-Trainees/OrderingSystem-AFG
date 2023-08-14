c8c0557f9b4d25fd2c428164324a73380fa5e6af
DBASYSTEM\Adrienne.Garimbao <adrienne.garimbao@dbaadvisory.com>
2023-08-14 14:02:10 +0800

    Added IAppService for Division

---
+++ aspnet-core/src/OrderingSystemAFG.Application/Divisions/IDivisionAppService.cs
@@ -0,0 +1,15 @@
+﻿using Abp.Application.Services;
+using OrderingSystemAFG.Divisions.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderingSystemAFG.Divisions
+{
+    public interface IDivisionAppService : IAsyncCrudAppService<DivisionDto, int, PagedDivisionResultRequestDto, CreateDivisionDto, DivisionDto>
+    {
+
+    }
+}
