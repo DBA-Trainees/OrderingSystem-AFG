422284dc5760f44dade27dcfb104c4fed92e955a
DBASYSTEM\Adrienne.Garimbao <adrienne.garimbao@dbaadvisory.com>
2023-08-14 10:29:56 +0800

    Added Permission Names for Vendor and Category

--- aspnet-core/src/OrderingSystemAFG.Core/Authorization/PermissionNames.cs
+++ aspnet-core/src/OrderingSystemAFG.Core/Authorization/PermissionNames.cs
@@ -3,11 +3,31 @@
     /* (1) */
     public static class PermissionNames
     {
-        public const string Pages_Tenants = "Pages.Tenants";
 
-        public const string Pages_Users = "Pages.Users";
-        public const string Pages_Users_Activation = "Pages.Users.Activation";
+        #region Default
+            public const string Pages_Tenants = "Pages.Tenants";
+            public const string Pages_Users = "Pages.Users";
+            public const string Pages_Users_Activation = "Pages.Users.Activation";
+            public const string Pages_Roles = "Pages.Roles";
+        #endregion
 
-        public const string Pages_Roles = "Pages.Roles";
+        #region Admin
+
+
+        #endregion
+
+
+        #region Vendor
+            public const string Pages_Users_Vendor = "Pages.User.Vendor";
+            public const string Pages_Vendor_Category = "Pages.Vendor.Category";
+
+        #endregion
+
+
+        #region Customers
+
+
+        #endregion
+
     }
 }
