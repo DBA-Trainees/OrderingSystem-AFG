51028c8770e2415ea04092eff3e66206855c51af
Adrienne Garimbao <Adriennegarimbao@hotmail.com>
2023-08-14 20:15:03 +0800

    Added IAppService for Food Type

---
+++ aspnet-core/src/OrderingSystemAFG.Application/FoodTypes/IFoodTypeAppService.cs
@@ -0,0 +1,17 @@
+﻿using Abp.Application.Services;
+using OrderingSystemAFG.FoodTypes.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderingSystemAFG.FoodTypes
+{
+    public interface IFoodTypeAppService : IAsyncCrudAppService<FoodTypeDto, int, PagedFoodTypeResultRequestDto, CreateFoodTypeDto, FoodTypeDto>
+    {
+        //Task<PagedResultDto<FoodTypeDto>> GetAllAsync(PagedFoodTypeResultRequestDto input);
+
+        //Task<List<FoodTypeDto>> GetAllTheListOfFoodTyoeFromDTO();
+    }
+}
