dc5b968be7b435800bb548458827de61fd19eba1
DBASYSTEM\Adrienne.Garimbao <adrienne.garimbao@dbaadvisory.com>
2023-08-14 14:41:22 +0800

    Imported the Blank CreateOrEdit Component into Division

--- angular/src/app/Admin-View/division/division.component.ts
+++ angular/src/app/Admin-View/division/division.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
+import { CreateOrEditDivisionComponent } from './create-or-edit-division/create-or-edit-division.component';
+
+
+
 @Component({
   selector: 'app-division',
   templateUrl: './division.component.html',
   styleUrls: ['./division.component.css']
 })
+
 export class DivisionComponent {
 
 }
