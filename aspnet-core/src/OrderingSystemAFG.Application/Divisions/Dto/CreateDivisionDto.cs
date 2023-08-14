a77294e7e68c8b121f1d0b54033b65ac4c867a92
DBASYSTEM\Adrienne.Garimbao <adrienne.garimbao@dbaadvisory.com>
2023-08-14 13:59:45 +0800

    Added DTO for Division

---
+++ aspnet-core/src/OrderingSystemAFG.Application/Divisions/Dto/CreateDivisionDto.cs
@@ -0,0 +1,18 @@
+﻿using Abp.AutoMapper;
+using OrderingSystemAFG.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderingSystemAFG.Divisions.Dto
+{
+    [AutoMapTo(typeof(Division))]
+
+    public class CreateDivisionDto
+    {
+        public string DivisionName { get; set; }
+
+    }
+}
