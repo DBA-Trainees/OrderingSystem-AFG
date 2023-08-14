8f49e02686c7065fa2ef80ed59b074e657c27beb
DBASYSTEM\Adrienne.Garimbao <adrienne.garimbao@dbaadvisory.com>
2023-08-14 13:48:49 +0800

    Added Entity Class for Division

---
+++ aspnet-core/src/OrderingSystemAFG.Core/Entities/Division.cs
@@ -0,0 +1,17 @@
+﻿using Abp.Domain.Entities.Auditing;
+using OrderingSystemAFG.Authorization.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderingSystemAFG.Entities
+{
+    public class Division : FullAuditedEntity<int>
+    {
+        /* List Columns */
+        public string DivisionName { get; set; } 
+
+    }
+}
