46f483a283067aa8a78a0fd26e7cf38ce1db3332
DBASYSTEM\Adrienne.Garimbao <adrienne.garimbao@dbaadvisory.com>
2023-08-14 11:18:02 +0800

    Added Starting Layout for the html component of Category

--- angular/src/app/Vendor-View/category/category.component.ts
+++ angular/src/app/Vendor-View/category/category.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 
 import { CreateOrEditCategoryComponent } from './create-or-edit-category/create-or-edit-category.component';
+import { appModuleAnimation } from '@shared/animations/routerTransition';
 
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./category.component.css']
+  styleUrls: ['./category.component.css'],
+  animations: [appModuleAnimation()],
 })
 
 
 export class CategoryComponent {
-  
+
+
 }
