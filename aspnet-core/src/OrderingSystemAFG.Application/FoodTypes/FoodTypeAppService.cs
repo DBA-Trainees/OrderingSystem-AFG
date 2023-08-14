45a14ae729bed039e1cb432e2a6123aa7d20e436
Adrienne Garimbao <Adriennegarimbao@hotmail.com>
2023-08-14 20:15:32 +0800

    Added AppService for Food Type

---
+++ aspnet-core/src/OrderingSystemAFG.Application/FoodTypes/FoodTypeAppService.cs
@@ -0,0 +1,42 @@
+﻿using Abp.Application.Services;
+using Abp.Application.Services.Dto;
+using Abp.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+using OrderingSystemAFG.Entities;
+using OrderingSystemAFG.FoodTypes.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderingSystemAFG.FoodTypes
+{
+    public class FoodTypeAppService : AsyncCrudAppService<Entities.Type, FoodTypeDto, int, PagedFoodTypeResultRequestDto, CreateFoodTypeDto, FoodTypeDto>, IFoodTypeAppService
+    {
+        private readonly IRepository<Entities.Type, int> _foodTypeIRepository;
+
+        public FoodTypeAppService(IRepository<Entities.Type, int> repository) : base(repository)
+        {
+            _foodTypeIRepository = repository;
+        }
+
+        public override async Task<PagedResultDto<FoodTypeDto>> GetAllAsync(PagedFoodTypeResultRequestDto input)
+        {
+            var foodTypeItems = await _foodTypeIRepository.GetAll()
+                .OrderByDescending(items => items.Id)
+                .Select(items => ObjectMapper.Map<FoodTypeDto>(items))
+                .ToListAsync();
+
+            return new PagedResultDto<FoodTypeDto>(foodTypeItems.Count(), foodTypeItems);
+        }
+
+        public async Task<List<FoodTypeDto>> GetAllTheListOfFoodTyoeFromDTO()
+        {
+            var foodTypeList = await _foodTypeIRepository.GetAllListAsync();
+
+            return ObjectMapper.Map<List<FoodTypeDto>>(foodTypeList);
+        }
+
+    }
+}
