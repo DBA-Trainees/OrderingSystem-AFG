1913088d147fd78aa2be90b43d6caf57cdcb87c0
DBASYSTEM\Adrienne.Garimbao <adrienne.garimbao@dbaadvisory.com>
2023-08-14 13:50:42 +0800

    Added DbContext for Division

--- aspnet-core/src/OrderingSystemAFG.EntityFrameworkCore/EntityFrameworkCore/OrderingSystemAFGDbContext.cs
+++ aspnet-core/src/OrderingSystemAFG.EntityFrameworkCore/EntityFrameworkCore/OrderingSystemAFGDbContext.cs
@@ -18,6 +18,8 @@ namespace OrderingSystemAFG.EntityFrameworkCore
 
         /* List of Tables  */
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Division> Divisions { get; set; }
+
 
 
     }
