29aa73928c0008d6366f4912d92979948dc5dc5a
DBASYSTEM\Adrienne.Garimbao <adrienne.garimbao@dbaadvisory.com>
2023-08-14 11:33:04 +0800

    Updated the index typescript component of Category

--- angular/src/app/Vendor-View/category/category.component.ts
+++ angular/src/app/Vendor-View/category/category.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Injector } from '@angular/core';
 
 import { CreateOrEditCategoryComponent } from './create-or-edit-category/create-or-edit-category.component';
 import { appModuleAnimation } from '@shared/animations/routerTransition';
+import { CategoryDto, CategoryDtoPagedResultDto, CategoryServiceProxy } from '@shared/service-proxies/service-proxies';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { PagedListingComponentBase, PagedRequestDto } from '@shared/paged-listing-component-base';
+import { finalize } from 'rxjs';
 
+class PagedCategoryDto extends PagedRequestDto //It must be always on the top of @Component
+{
+    keyword: string;
+    IsActive: boolean | null;
+}
 
 @Component({
   selector: 'app-category',
@@ -12,7 +21,109 @@ import { appModuleAnimation } from '@shared/animations/routerTransition';
 })
 
 
-export class CategoryComponent {
 
+export class CategoryComponent extends PagedListingComponentBase<CategoryDto>{
+  
+
+  categoryItems: CategoryDto[] = [];
+  totalRecords: number = 0;
+  loading: boolean = false;
+  request: CategoryDtoPagedResultDto;
+  keyword: '';
+  isActive: boolean | null;
+  advancedFiltersVisible = false;
+
+
+  constructor(
+    private injector: Injector,
+    private _categoryServiceProxy: CategoryServiceProxy,
+    private _categoryModalService: BsModalService,
+  )
+  {
+    super(injector);
+  }
+
+  ShowCreateCategory() : void
+  {
+    this.CreateCategory();
+  }
+  
+  ShowEditCategory(id) : void
+  {
+    this.EditCategory(id);
+  }
+  
+  
+  protected list(request: PagedCategoryDto, pageNumber: number, finishedCallback: Function): void {
+    
+    this._categoryServiceProxy.getAll(
+      request.keyword,
+      request.IsActive,
+      request.skipCount,
+      request.maxResultCount
+    )
+    .pipe(
+      finalize(() => {
+        finishedCallback();
+      })
+    )
+    .subscribe((result: CategoryDtoPagedResultDto) => {
+      this.categoryItems = result.items;
+      this.showPaging(result, pageNumber);
+    });
+    
+  }
+
+  protected delete(categorySelectedItem: CategoryDto): void {
+    
+    abp.message.confirm(this.l('DeleteMessage', categorySelectedItem.categoryName), undefined,
+      (condition: boolean) => {
+        if (condition)
+        {
+            this._categoryServiceProxy.delete(categorySelectedItem.id).subscribe(() => {
+              abp.notify.success(this.l('SuccessfullyDeleted'));
+              this.refresh();
+
+            });
+        }
+      }
+    );
+
+  }
+
+  private CreateCategory(id?: number): void
+  {
+    let showCreateCategoryComponent: BsModalRef
+
+    if(!id)
+    {
+      showCreateCategoryComponent = this._categoryModalService.show(CreateOrEditCategoryComponent,
+        {
+            class: 'modal-lg',
+        });
+
+        showCreateCategoryComponent.content.onSave.subscribe(() => {
+          this.refresh();
+        })
+    }
+
+  }
+
+  private EditCategory(id?: number): void
+  {
+    let showEditCategoryComponent: BsModalRef
+
+    showEditCategoryComponent = this._categoryModalService.show(CreateOrEditCategoryComponent, 
+      {
+          class: 'modal-lg',
+          initialState: {id: id},
+      });
+
+      showEditCategoryComponent.content.onSave.subscribe(() => {
+        this.refresh();
+      })
+
+  }
+  
 
 }
