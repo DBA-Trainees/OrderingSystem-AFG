70766e2263fce41050b8592c1f6db93408a1bae3
DBASYSTEM\Adrienne.Garimbao <adrienne.garimbao@dbaadvisory.com>
2023-08-14 10:59:04 +0800

    Imported the Blank CreateOrEdit Component into Category

--- angular/src/app/Vendor-View/category/category.component.ts
+++ angular/src/app/Vendor-View/category/category.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
+import { CreateOrEditCategoryComponent } from './create-or-edit-category/create-or-edit-category.component';
+
+
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
   styleUrls: ['./category.component.css']
 })
-export class CategoryComponent {
 
+
+export class CategoryComponent {
+  
 }
