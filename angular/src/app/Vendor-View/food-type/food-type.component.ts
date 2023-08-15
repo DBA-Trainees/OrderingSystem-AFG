cbae239f4e41e8c8a384eccf6df9928ec7f4fb01
DBASYSTEM\Adrienne.Garimbao <adrienne.garimbao@dbaadvisory.com>
2023-08-15 09:25:38 +0800

    Imported the Blank CreateOrEdit Component into Food Type

--- angular/src/app/Vendor-View/food-type/food-type.component.ts
+++ angular/src/app/Vendor-View/food-type/food-type.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { CreateOrEditFoodtypeComponent } from './create-or-edit-foodtype/create-or-edit-foodtype.component';
+
 @Component({
   selector: 'app-food-type',
   templateUrl: './food-type.component.html',
@@ -8,3 +10,4 @@ import { Component } from '@angular/core';
 export class FoodTypeComponent {
 
 }
+
