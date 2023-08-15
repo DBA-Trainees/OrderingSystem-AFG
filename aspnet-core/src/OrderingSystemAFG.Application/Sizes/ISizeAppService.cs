e69451e7a9fb55e239e9682d39f0a33171a6db11
DBASYSTEM\Adrienne.Garimbao <adrienne.garimbao@dbaadvisory.com>
2023-08-15 13:16:04 +0800

    Added IAppService for Size

---
+++ aspnet-core/src/OrderingSystemAFG.Application/Sizes/ISizeAppService.cs
@@ -0,0 +1,18 @@
+﻿using Abp.Application.Services;
+using Abp.Application.Services.Dto;
+using OrderingSystemAFG.Sizes.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderingSystemAFG.Sizes
+{
+    public interface ISizeAppService : IAsyncCrudAppService<SizeDto, int, PagedSizeResultRequestDto, CreateSizeDto, SizeDto>
+    {
+        //Task<PagedResultDto<SizeDto>> GetAllAsync(PagedSizeResultRequestDto input);
+
+        //Task<List<SizeDto>> GetAllTheListOfSizeFromDTO();
+    }
+}
