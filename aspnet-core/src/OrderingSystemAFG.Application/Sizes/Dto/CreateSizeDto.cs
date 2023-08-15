718832553d9752b4b14b97529a764ff4436dc3a1
DBASYSTEM\Adrienne.Garimbao <adrienne.garimbao@dbaadvisory.com>
2023-08-15 13:15:47 +0800

    Added DTO for Size

---
+++ aspnet-core/src/OrderingSystemAFG.Application/Sizes/Dto/CreateSizeDto.cs
@@ -0,0 +1,16 @@
+﻿using Abp.AutoMapper;
+using OrderingSystemAFG.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderingSystemAFG.Sizes.Dto
+{
+    [AutoMapTo(typeof(Size))]
+    public class CreateSizeDto
+    {
+        public string SizeName { get; set; }
+    }
+}
---
+++ aspnet-core/src/OrderingSystemAFG.Application/Sizes/Dto/SizeDto.cs
@@ -0,0 +1,20 @@
+﻿using Abp.Application.Services.Dto;
+using Abp.AutoMapper;
+using OrderingSystemAFG.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderingSystemAFG.Sizes.Dto
+{
+    [AutoMapFrom(typeof(Size))]
+    [AutoMapTo(typeof(Size))]
+
+    public class SizeDto : EntityDto<int>
+    {
+        public string SizeName { get; set; }
+
+    }
+}
