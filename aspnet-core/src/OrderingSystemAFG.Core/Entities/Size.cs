6162b7310269294b97f60362e605029de6cf48b1
DBASYSTEM\Adrienne.Garimbao <adrienne.garimbao@dbaadvisory.com>
2023-08-15 12:52:42 +0800

    Added Entity Class for Size

---
+++ aspnet-core/src/OrderingSystemAFG.Core/Entities/Size.cs
@@ -0,0 +1,16 @@
+﻿using Abp.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderingSystemAFG.Entities
+{
+    public class Size : FullAuditedEntity<int>
+    {
+        /* List Columns */
+        public string SizeName { get; set; }
+
+    }
+}
