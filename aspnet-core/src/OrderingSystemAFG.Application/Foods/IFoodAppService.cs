7592b73a2226cef08381d9c4eddae4345bfdb588
DBASYSTEM\Adrienne.Garimbao <adrienne.garimbao@dbaadvisory.com>
2023-08-16 08:40:38 +0800

    Added IAppService for Food

---
+++ aspnet-core/src/OrderingSystemAFG.Application/Foods/IFoodAppService.cs
@@ -0,0 +1,18 @@
+﻿using Abp.Application.Services;
+using Abp.Application.Services.Dto;
+using OrderingSystemAFG.Foods.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderingSystemAFG.Foods
+{
+    public interface IFoodAppService : IAsyncCrudAppService<FoodDto, int, PagedFoodResultRequestDto, CreateFoodDto, FoodDto>
+    {
+        //Task<PagedResultDto<FoodDto>> GetAllAsync(PagedFoodResultRequestDto input);
+        //Task<List<FoodDto>> GetAllTheListOfFood();
+        //Task<PagedResultDto<FoodDto>> GetAllTheFoodItemWhereAvailabilityIsTrue(PagedFoodResultRequestDto input);
+    }
+}
