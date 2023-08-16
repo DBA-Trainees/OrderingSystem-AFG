5fc1675637ae75c2d9b56579ae440fbf71ac9325
DBASYSTEM\Adrienne.Garimbao <adrienne.garimbao@dbaadvisory.com>
2023-08-16 12:38:38 +0800

    Added IAppService for Customer Order

---
+++ aspnet-core/src/OrderingSystemAFG.Application/CustomerOrders/ICustomerOrderAppService.cs
@@ -0,0 +1,17 @@
+﻿using Abp.Application.Services;
+using Abp.Application.Services.Dto;
+using OrderingSystemAFG.CustomerOrders.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderingSystemAFG.CustomerOrders
+{
+    public interface ICustomerOrderAppService : IAsyncCrudAppService<CustomerOrderDto, int, PagedCustomerOrderResultRequestDto, CreateCustomerOrderDto, CustomerOrderDto>
+    {
+        //Task<PagedResultDto<CustomerOrderDto>> GetAllAsync(PagedCustomerOrderResultRequestDto input);
+        //Task<PagedResultDto<CustomerOrderDto>> GetAllTheListOfOrderIncludingFoodThenCategoryThenSizeThenDivision(PagedCustomerOrderResultRequestDto input);
+    }
+}
