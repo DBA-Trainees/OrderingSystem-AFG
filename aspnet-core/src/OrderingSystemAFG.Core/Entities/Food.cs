71f4e4d6fdab447d19a5f376f9b9e3b595b682e2
DBASYSTEM\Adrienne.Garimbao <adrienne.garimbao@dbaadvisory.com>
2023-08-16 08:13:24 +0800

    Added Entity Class for Food

---
+++ aspnet-core/src/OrderingSystemAFG.Core/Entities/Food.cs
@@ -0,0 +1,27 @@
+﻿using Abp.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderingSystemAFG.Entities
+{
+    public class Food : FullAuditedEntity<int>
+    {
+        /* List Columns */
+        public string FoodName { get; set; } 
+        public double Price { get; set; } 
+        public byte[] Image { get; set; } 
+        public string ImageName { get; set; } 
+        public string ImageFileType { get; set; } 
+        public bool Availability { get; set; } 
+        public int TotalStock { get; set; } 
+        public int? TypeId { get; set; } 
+        public Type Type { get; set; } 
+        public int? SizeId { get; set; } 
+        public Size Size { get; set; }
+        
+
+    }
+}
