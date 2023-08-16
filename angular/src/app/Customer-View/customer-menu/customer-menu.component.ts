4dff07e5575f28d21398901261ba6473fa1ad0a3
DBASYSTEM\Adrienne.Garimbao <adrienne.garimbao@dbaadvisory.com>
2023-08-16 13:08:15 +0800

    Imported the Blank AddToCart Component into Customer Menu

--- angular/src/app/Customer-View/customer-menu/customer-menu.component.ts
+++ angular/src/app/Customer-View/customer-menu/customer-menu.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+import { AddToCartComponent } from './add-to-cart/add-to-cart.component';
+
 @Component({
   selector: 'app-customer-menu',
   templateUrl: './customer-menu.component.html',
   styleUrls: ['./customer-menu.component.css']
 })
+
 export class CustomerMenuComponent {
 
 }
