9d8a1514e0891b4edba0e0419eb6bc737b63ef9d
DBASYSTEM\Adrienne.Garimbao <adrienne.garimbao@dbaadvisory.com>
2023-08-16 11:32:24 +0800

    Added Entity Class for Customer Order

---
+++ aspnet-core/src/OrderingSystemAFG.Core/Entities/CustomerOrder.cs
@@ -0,0 +1,30 @@
+﻿using Abp.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderingSystemAFG.Entities
+{
+    public class CustomerOrder : FullAuditedEntity<int>
+    {
+        /* List Columns */
+
+        public int? FoodId { get; set; } 
+        public Food Food { get; set; } 
+        public string CustomerName { get; set; } 
+        public int? DivisionId { get; set; } 
+        public Division Division { get; set; } 
+        public int? CategoryId { get; set; } 
+        public Category Category { get; set; } 
+        public string Notes { get; set; } 
+        public int? SizeId { get; set; } 
+        public Size Size { get; set; } 
+        public int TotalQuantityOfOrder { get; set; } 
+        public bool OrderStatus { get; set; } 
+        public DateTime? DateAndTimeOrderIsPlaced { get; set; } 
+        public DateTime? DateAndTimeOrderIsRecieved { get; set; } 
+
+    }
+}
