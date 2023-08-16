ea7580ada4b158d2608007bad23ec62cbd63a140
DBASYSTEM\Adrienne.Garimbao <adrienne.garimbao@dbaadvisory.com>
2023-08-16 08:48:13 +0800

    Added Providers for ServiceProxy of Food

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -42,7 +42,7 @@ import { SidebarMenuComponent } from './layout/sidebar-menu.component';
 
 // #region (1) Import the component here
 import { CategoryComponent } from './Vendor-View/category/category.component';
-import { CategoryServiceProxy, CustomerServiceProxy, DivisionServiceProxy, FoodTypeServiceProxy, SizeServiceProxy } from '@shared/service-proxies/service-proxies';
+import { CategoryServiceProxy, CustomerServiceProxy, DivisionServiceProxy, FoodServiceProxy, FoodTypeServiceProxy, SizeServiceProxy } from '@shared/service-proxies/service-proxies';
 import { CreateOrEditCategoryComponent } from './Vendor-View/category/create-or-edit-category/create-or-edit-category.component';
 import { DivisionComponent } from './Admin-View/division/division.component';
 import { CreateOrEditDivisionComponent } from './Admin-View/division/create-or-edit-division/create-or-edit-division.component';
@@ -125,6 +125,7 @@ import { FoodListComponent } from './Vendor-View/food-list/food-list.component';
         FoodTypeServiceProxy,
         SizeServiceProxy,
         CustomerServiceProxy,
+        FoodServiceProxy,
         
         // #endregion
 
