300a33e02c48a694518f9a1b993fa827b1fbb990
DBASYSTEM\Adrienne.Garimbao <adrienne.garimbao@dbaadvisory.com>
2023-09-01 13:12:08 +0800

    Added migration to update Customer Order Table

---
+++ aspnet-core/src/OrderingSystemAFG.EntityFrameworkCore/Migrations/20230901051039_Updated_CustomerOrder_Table_and_added_GrandTotal_Column.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OrderingSystemAFG.Migrations
+{
+    /// <inheritdoc />
+    public partial class Updated_CustomerOrder_Table_and_added_GrandTotal_Column : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "GrandTotal",
+                table: "CustomersOrders",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GrandTotal",
+                table: "CustomersOrders");
+        }
+    }
+}
