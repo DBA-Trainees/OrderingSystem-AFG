7446a2679f4e9e6f561e5e750ead66002cecb736
Adrienne Garimbao <Adrienne.Garimbao@dbaadvisory.com>
2023-09-07 11:04:53 +0800

    Updated customer-edit order component then customer-checkout component

--- angular/src/app/Customer-View/customer-checkout/customer-checkout.component.ts
+++ angular/src/app/Customer-View/customer-checkout/customer-checkout.component.ts
@@ -1,10 +1,88 @@
-import { Component } from '@angular/core';
+import { Component, Injector } from '@angular/core';
+import { Router } from '@angular/router';
+import { appModuleAnimation } from '@shared/animations/routerTransition';
+import { PagedListingComponentBase, PagedRequestDto } from '@shared/paged-listing-component-base';
+import { CustomerOrderDto, CustomerOrderDtoPagedResultDto, CustomerOrderServiceProxy } from '@shared/service-proxies/service-proxies';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { finalize } from 'rxjs';
+
+class PagedOrderDto extends PagedRequestDto
+{
+    keyword: string;
+    IsActive: boolean | null;
+}
 
 @Component({
   selector: 'app-customer-checkout',
   templateUrl: './customer-checkout.component.html',
-  styleUrls: ['./customer-checkout.component.css']
+  styleUrls: ['./customer-checkout.component.css'],
+  animations: [appModuleAnimation()],
 })
-export class CustomerCheckoutComponent {
+
+export class CustomerCheckoutComponent extends PagedListingComponentBase<CustomerOrderDto> {
+  
+  checkoutItems: CustomerOrderDto[] = [];
+  orderDto = new CustomerOrderDto();
+
+  grandTotal: number = 0;
+
+  constructor(
+      injector: Injector,
+      private _orderServiceProxy: CustomerOrderServiceProxy,
+      private _checkoutModalService: BsModalService,
+      private checkoutRouter: Router,
+  )
+  {
+      super(injector);
+  }
+  
+  
+  protected list(request: PagedOrderDto, pageNumber: number, finishedCallback: Function): void {
+
+      this._orderServiceProxy.getAllTheListOfOrderIncludingFoodThenCategoryThenSizeThenDivision(
+          request.keyword,
+          request.IsActive,
+          request.skipCount,
+          request.maxResultCount
+      )
+      .pipe(
+          finalize(() => {
+            finishedCallback();
+            this.GrandTotal();
+          })
+      )
+      .subscribe((result: CustomerOrderDtoPagedResultDto) => {
+          this.checkoutItems = result.items;
+          this.showPaging(result, pageNumber);
+      });
+
+
+  }
+
+  protected delete(entity: CustomerOrderDto): void {
+    throw new Error('Method not implemented.');
+  }
+
+  UpdatedSubTotal(orderDto : CustomerOrderDto): number
+  {
+      let originalAmmount = orderDto.food?.price;
+      let quantity = orderDto.totalQuantityOfOrder;
+      let subTotal = 0;
+
+      subTotal = originalAmmount * quantity;
+
+      return subTotal;
+  }
+
+  GrandTotal(): number
+  {
+      this.grandTotal = this.checkoutItems.reduce((total, orderDto) => {
+            return total + this.UpdatedSubTotal(orderDto)}, 0
+      );   
+
+      return this.grandTotal;
+
+  }
+
 
 }
