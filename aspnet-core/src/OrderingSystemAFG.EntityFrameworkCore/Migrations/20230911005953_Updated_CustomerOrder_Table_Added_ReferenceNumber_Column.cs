bd63c785668149b781aec94b2a20abfc8f76ee07
Adrienne Garimbao <Adrienne.Garimbao@dbaadvisory.com>
2023-09-11 09:02:29 +0800

    Added new Migration for Customer Order

---
+++ aspnet-core/src/OrderingSystemAFG.EntityFrameworkCore/Migrations/20230911005953_Updated_CustomerOrder_Table_Added_ReferenceNumber_Column.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OrderingSystemAFG.Migrations
+{
+    /// <inheritdoc />
+    public partial class Updated_CustomerOrder_Table_Added_ReferenceNumber_Column : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "ReferenceNumber",
+                table: "CustomersOrders",
+                type: "uniqueidentifier",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReferenceNumber",
+                table: "CustomersOrders");
+        }
+    }
+}
