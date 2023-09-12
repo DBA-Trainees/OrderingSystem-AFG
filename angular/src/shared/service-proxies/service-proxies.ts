9e30f8d132de0110838127b470bad4f2d4d6c8ed
Adrienne Garimbao <Adrienne.Garimbao@dbaadvisory.com>
2023-09-12 09:02:34 +0800

    Updated the service-proxy

--- angular/src/shared/service-proxies/service-proxies.ts
+++ angular/src/shared/service-proxies/service-proxies.ts
@@ -1206,6 +1206,62 @@ export class CustomerOrderServiceProxy {
     }
 
     /**
+     * @param body (optional) 
+     * @return Success
+     */
+    updateStatusNumberIntoFour(body: CustomerOrderDto | undefined): Observable<CustomerOrderDto> {
+        let url_ = this.baseUrl + "/api/services/app/CustomerOrder/UpdateStatusNumberIntoFour";
+        url_ = url_.replace(/[?&]$/, "");
+
+        const content_ = JSON.stringify(body);
+
+        let options_ : any = {
+            body: content_,
+            observe: "response",
+            responseType: "blob",
+            headers: new HttpHeaders({
+                "Content-Type": "application/json-patch+json",
+                "Accept": "text/plain"
+            })
+        };
+
+        return this.http.request("put", url_, options_).pipe(_observableMergeMap((response_ : any) => {
+            return this.processUpdateStatusNumberIntoFour(response_);
+        })).pipe(_observableCatch((response_: any) => {
+            if (response_ instanceof HttpResponseBase) {
+                try {
+                    return this.processUpdateStatusNumberIntoFour(response_ as any);
+                } catch (e) {
+                    return _observableThrow(e) as any as Observable<CustomerOrderDto>;
+                }
+            } else
+                return _observableThrow(response_) as any as Observable<CustomerOrderDto>;
+        }));
+    }
+
+    protected processUpdateStatusNumberIntoFour(response: HttpResponseBase): Observable<CustomerOrderDto> {
+        const status = response.status;
+        const responseBlob =
+            response instanceof HttpResponse ? response.body :
+            (response as any).error instanceof Blob ? (response as any).error : undefined;
+
+        let _headers: any = {}; if (response.headers) { for (let key of response.headers.keys()) { _headers[key] = response.headers.get(key); }}
+        if (status === 200) {
+            return blobToText(responseBlob).pipe(_observableMergeMap((_responseText: string) => {
+            let result200: any = null;
+            let resultData200 = _responseText === "" ? null : JSON.parse(_responseText, this.jsonParseReviver);
+            result200 = CustomerOrderDto.fromJS(resultData200);
+            return _observableOf(result200);
+            }));
+        } else if (status !== 200 && status !== 204) {
+            return blobToText(responseBlob).pipe(_observableMergeMap((_responseText: string) => {
+            return throwException("An unexpected server error occurred.", status, _responseText, _headers);
+            }));
+        }
+        return _observableOf(null as any);
+    }
+
+    /**
      * @param id (optional) 
      * @return Success
      */
