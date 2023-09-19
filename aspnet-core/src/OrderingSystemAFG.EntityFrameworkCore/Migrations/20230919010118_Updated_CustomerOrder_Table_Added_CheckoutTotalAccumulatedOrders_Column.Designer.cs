b9b7405c480a16fd04d01970c0b0e28de6b129a8
Adrienne Garimbao <Adrienne.Garimbao@dbaadvisory.com>
2023-09-19 09:10:36 +0800

    Updated Migration for CustomerOrder Table

--- aspnet-core/src/OrderingSystemAFG.EntityFrameworkCore/Migrations/20230919010118_Updated_CustomerOrder_Table_Added_CheckoutTotalAccumulatedOrders_Column.Designer.cs
+++ aspnet-core/src/OrderingSystemAFG.EntityFrameworkCore/Migrations/20230919010118_Updated_CustomerOrder_Table_Added_CheckoutTotalAccumulatedOrders_Column.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OrderingSystemAFG.EntityFrameworkCore;
 
@@ -11,9 +12,11 @@ using OrderingSystemAFG.EntityFrameworkCore;
 namespace OrderingSystemAFG.Migrations
 {
     [DbContext(typeof(OrderingSystemAFGDbContext))]
-    partial class OrderingSystemAFGDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230919010118_Updated_CustomerOrder_Table_Added_CheckoutTotalAccumulatedOrders_Column")]
+    partial class Updated_CustomerOrder_Table_Added_CheckoutTotalAccumulatedOrders_Column
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ aspnet-core/src/OrderingSystemAFG.EntityFrameworkCore/Migrations/20230919010118_Updated_CustomerOrder_Table_Added_CheckoutTotalAccumulatedOrders_Column.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OrderingSystemAFG.Migrations
+{
+    /// <inheritdoc />
+    public partial class Updated_CustomerOrder_Table_Added_CheckoutTotalAccumulatedOrders_Column : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CheckoutTotalAccumulatedOrders",
+                table: "CustomersOrders",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CheckoutTotalAccumulatedOrders",
+                table: "CustomersOrders");
+        }
+    }
+}
