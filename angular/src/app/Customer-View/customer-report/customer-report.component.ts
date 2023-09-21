64768a4eda8b8d9e0fe614ac573a27a20ed5d6c9
Adrienne Garimbao <Adrienne.Garimbao@dbaadvisory.com>
2023-09-21 14:14:12 +0800

    Updated the html and typescript component(s)

--- angular/src/app/Customer-View/customer-report/customer-report.component.ts
+++ angular/src/app/Customer-View/customer-report/customer-report.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, Injector } from '@angular/core';
+import { appModuleAnimation } from '@shared/animations/routerTransition';
+import { PagedListingComponentBase, PagedRequestDto } from '@shared/paged-listing-component-base';
+import { CustomerOrderDto, CustomerOrderDtoPagedResultDto, CustomerOrderServiceProxy } from '@shared/service-proxies/service-proxies';
+import { finalize } from 'rxjs';
+
+class PagedOrderDtoCustomer extends PagedRequestDto
+{
+    keyword: string;
+    IsActive: boolean | null;
+
+}
 
 @Component({
   selector: 'app-customer-report',
   templateUrl: './customer-report.component.html',
-  styleUrls: ['./customer-report.component.css']
+  styleUrls: ['./customer-report.component.css'],
+  animations: [appModuleAnimation()],
 })
-export class CustomerReportComponent {
+
+export class CustomerReportComponent extends PagedListingComponentBase<CustomerOrderDto>
+{
+
+  orderList: CustomerOrderDto[] = [];
+  orderDtoCustomer = new CustomerOrderDto();
+
+  keyword = "";
+  isActive: boolean | null;
+  advancedFiltersVisible = false;
+
+  constructor(
+      injector: Injector,
+      private _orderServiceProxyVendor: CustomerOrderServiceProxy,
+  )
+  {
+      super(injector);
+  }
+
+
+  protected list(request: PagedOrderDtoCustomer, pageNumber: number, finishedCallback: Function): void {
+    
+    this._orderServiceProxyVendor.getAllPaidOrders(
+        request.keyword,
+        request.IsActive,
+        request.skipCount,
+        request.maxResultCount,
+    )
+    .pipe(
+        finalize(() => {
+            finishedCallback();
+        })
+    )
+    .subscribe((result: CustomerOrderDtoPagedResultDto) => {
+        this.orderList = result.items;
+        this.showPaging(result, pageNumber);
+    });
+
+
+  }
+
+  protected delete(entity: CustomerOrderDto): void {
+    //throw new Error('Method not implemented.');
+  }
+
+  
+
 
 }
+
