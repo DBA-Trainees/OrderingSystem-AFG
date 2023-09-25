a489f83b913d1985775948dfa329784e546029d1
Adrienne Garimbao <Adrienne.Garimbao@dbaadvisory.com>
2023-09-25 10:02:13 +0800

    Updated the vendor-reports html and typescript components

--- angular/src/app/Vendor-View/vendor-reports/vendor-reports.component.ts
+++ angular/src/app/Vendor-View/vendor-reports/vendor-reports.component.ts
@@ -1,10 +1,82 @@
-import { Component } from '@angular/core';
+import { Component, Injector } from '@angular/core';
+import { PagedListingComponentBase, PagedRequestDto } from '@shared/paged-listing-component-base';
+import { CustomerOrderDto, CustomerOrderDtoPagedResultDto, CustomerOrderServiceProxy } from '@shared/service-proxies/service-proxies';
+import { result } from 'lodash-es';
+import { finalize } from 'rxjs';
+
+class PagedOrderDtoVendor extends PagedRequestDto
+{
+    keyword: string;
+    IsActive: boolean | null;
+
+}
+
 
 @Component({
   selector: 'app-vendor-reports',
   templateUrl: './vendor-reports.component.html',
   styleUrls: ['./vendor-reports.component.css']
 })
-export class VendorReportsComponent {
+
+
+export class VendorReportsComponent extends PagedListingComponentBase<CustomerOrderDto>
+{
+
+  orderList: CustomerOrderDto[] = [];
+  totalSales: number = 0; 
+
+
+  keyword = "";
+  isActive: boolean | null;
+  advancedFiltersVisible = false;
+
+
+  constructor(
+    injector: Injector,
+    private _orderServiceProxyVendor: CustomerOrderServiceProxy,
+  )
+  {
+      super(injector);
+  }
+
+
+  protected list(request: PagedOrderDtoVendor, pageNumber: number, finishedCallback: Function): void {
+
+      this._orderServiceProxyVendor.getAllPaidOrders(
+        request.keyword,
+        request.IsActive,
+        request.skipCount,
+        request.maxResultCount,
+      )
+      .pipe(
+          finalize(() => {
+              finishedCallback();
+              this.UpdatedTotalSales();
+
+          })
+      )
+      .subscribe((result: CustomerOrderDtoPagedResultDto) => {
+          this.orderList = result.items;
+          this.showPaging(result, pageNumber);
+      });
+
+  }
+
+
+  protected delete(entity: CustomerOrderDto): void {
+      //throw new Error('Method not implemented.');
+  }
+
+  UpdatedTotalSales(): number
+  {
+      this.totalSales = this.orderList.reduce((total, orderDto) => {
+            return total + orderDto.grandTotal}, 0
+      );   
+
+      return this.totalSales;
+
+  }
+
+
 
 }
